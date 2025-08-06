d43fac6e1297ddd3da9a03645799cb77e21b62df
A5M1 <101961638+A5M1@users.noreply.github.com>
2025-08-06 12:48:19 -0700

    Update gm_ui.js

--- src/gm_ui.js
+++ src/gm_ui.js
@@ -1,31 +1,9 @@
-window.GMUI = {
-	addStyle: function(e, t) {
-		t = t || document;
-		const n = t.createElement("style");
-		n.textContent = e, (t.head || t.documentElement)
-			.appendChild(n)
-	}
+(function(){
+window.GMUI={
+addStyle:function(e,t){t=t||document;const n=t.createElement("style");n.textContent=e,(t.head||t.documentElement).appendChild(n)}
 };
-window.UIState = function() {
-	let e, t;
 
-	function
-	n(o) {
-		t && (o ? (document.head.contains(t) || document.head.appendChild(t), t.disabled = !1) : t
-			.disabled = !0)
-	}
-	return {
-		init: function(o) {
-			e = o.key, t = document.createElement("style"), t.textContent = o.css, document
-				.addEventListener("keydown", function(t) {
-					t.altKey && t.key.toLowerCase() === o.hotkey && function() {
-						const t = !("true" === localStorage.getItem(e));
-						localStorage.setItem(e, String(t)), n(t)
-					}()
-				}), document.addEventListener("DOMContentLoaded", function() {
-					n("true" === localStorage.getItem(e)), o.bodyClass && document.body.classList
-						.add(o.bodyClass)
-				})
-		}
-	}
-}();
+window.UIState=function(){let e,t;function n(o){t&&(o?(document.head.contains(t)||document.head.appendChild(t),t.disabled=!1):t.disabled=!0)}return{
+init:function(o){e=o.key,t=document.createElement("style"),t.textContent=o.css,document.addEventListener("keydown",function(t){t.altKey&&t.key.toLowerCase()===o.hotkey&&function(){const t=!("true"===localStorage.getItem(e));localStorage.setItem(e,String(t)),n(t)}()}),document.addEventListener("DOMContentLoaded",function(){n("true"===localStorage.getItem(e)),o.bodyClass&&document.body.classList.add(o.bodyClass)})}
+}}();
+})();
