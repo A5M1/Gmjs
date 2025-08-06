5d94327ce5a7dd06de1fdab67dc7e2435417db59
A5M1 <101961638+A5M1@users.noreply.github.com>
2025-08-06 12:50:43 -0700

    Update gm_ui.js

--- src/gm_ui.js
+++ src/gm_ui.js
@@ -1,9 +1,40 @@
-(function(){
-window.GMUI={
-addStyle:function(e,t){t=t||document;const n=t.createElement("style");n.textContent=e,(t.head||t.documentElement).appendChild(n)}
-};
+// ==UserScript==
+// @name         CB Utils
+// @namespace    abn
+// @version      1.0
+// @description  Utility functions for Chaturbate scripts
+// @match        *://*
+// @grant        unsafeWindow
+// ==/UserScript==
+(function() {
+	unsafeWindow.GMUI = {
+		addStyle: function(e, t) {
+			t = t || document;
+			const n = t.createElement("style");
+			n.textContent = e, (t.head || t.documentElement)
+				.appendChild(n)
+		}
+	};
+	unsafeWindow.UIState = function() {
+		let e, t;
 
-window.UIState=function(){let e,t;function n(o){t&&(o?(document.head.contains(t)||document.head.appendChild(t),t.disabled=!1):t.disabled=!0)}return{
-init:function(o){e=o.key,t=document.createElement("style"),t.textContent=o.css,document.addEventListener("keydown",function(t){t.altKey&&t.key.toLowerCase()===o.hotkey&&function(){const t=!("true"===localStorage.getItem(e));localStorage.setItem(e,String(t)),n(t)}()}),document.addEventListener("DOMContentLoaded",function(){n("true"===localStorage.getItem(e)),o.bodyClass&&document.body.classList.add(o.bodyClass)})}
-}}();
+		function n(o) {
+			t && (o ? (document.head.contains(t) || document.head.appendChild(t), t.disabled = !1) : t
+				.disabled = !0)
+		}
+		return {
+			init: function(o) {
+				e = o.key, t = document.createElement("style"), t.textContent = o.css, document
+					.addEventListener("keydown", function(t) {
+						t.altKey && t.key.toLowerCase() === o.hotkey && function() {
+							const t = !("true" === localStorage.getItem(e));
+							localStorage.setItem(e, String(t)), n(t)
+						}()
+					}), document.addEventListener("DOMContentLoaded", function() {
+						n("true" === localStorage.getItem(e)), o.bodyClass && document.body
+							.classList.add(o.bodyClass)
+					})
+			}
+		}
+	}();
 })();
